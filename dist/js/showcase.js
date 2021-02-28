cc93d0ed5d226a78d8d9eda58df6910d3ab2a357
francism98 <francismanubag@gmail.com>
2021-03-01 00:11:04 +0800

    changes to projects design

--- dist/js/showcase.js
+++ dist/js/showcase.js
@@ -1,21 +1,20 @@
+const containerProject = Array.from(document.getElementsByClassName('container-project'));
+  const projectContainer = document.querySelector('#projects .container');
+
+  containerProject[0].addEventListener('mouseover', () => {
+    projectContainer.classList.add('project1');
+    console.log(projectContainer);
+  })
+  containerProject[0].addEventListener('mouseleave', () => {
+    projectContainer.classList.remove('project1');
+  })
+
+  containerProject[1].addEventListener('mouseover', () => {
+    projectContainer.classList.add('project2');
+    console.log(projectContainer);
+  })
+  containerProject[1].addEventListener('mouseleave', () => {
+    projectContainer.classList.remove('project2');
+  })
 
 
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-document.addEventListener('DOMContentLoaded', init);
