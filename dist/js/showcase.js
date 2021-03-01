2374899fb0af97d2b55e82617e697172c078a6fa
francism98 <francismanubag@gmail.com>
2021-03-01 11:43:26 +0800

    about design change, responsiveness tweaks, projects design change, added current-progress, added footer

--- dist/js/showcase.js
+++ dist/js/showcase.js
@@ -1,20 +1,42 @@
 const containerProject = Array.from(document.getElementsByClassName('container-project'));
-  const projectContainer = document.querySelector('#projects .container');
+const projectContainer = document.querySelector('#projects .container');
+const projectPreview1 = document.querySelector('.project-preview1');
+const projectPreview2 = document.querySelector('.project-preview2');
+const projectPreviewImg1 = document.querySelector('.project-preview1 img');
+const projectPreviewImg2 = document.querySelector('.project-preview2 img');
+const projectSamples = document.querySelector('.project-samples');
 
   containerProject[0].addEventListener('mouseover', () => {
+    for (items of containerProject) {
+      items.style.height = '10%';
+    }
+    containerProject[0].style.height = '80%';
+    projectContainer.classList.remove('project2');
     projectContainer.classList.add('project1');
-    console.log(projectContainer);
+    projectPreview2.classList.remove('show');
+    projectPreview1.classList.add('show');
+    console.log(containerProject[0]);
   })
-  containerProject[0].addEventListener('mouseleave', () => {
+  projectSamples.addEventListener('mouseleave', () => {
     projectContainer.classList.remove('project1');
+    projectContainer.classList.remove('project2');
+    projectPreview1.classList.remove('show');
+    projectPreview2.classList.remove('show');
+    for (items of containerProject) {
+      items.style.height = '25%';
+    }
+    projectSamples.style.justifyContent = 'flex-start';
   })
 
   containerProject[1].addEventListener('mouseover', () => {
+    for (items of containerProject) {
+      items.style.height = '10%';
+    }
+    containerProject[1].style.height = '80%';
+    projectContainer.classList.remove('project1');
     projectContainer.classList.add('project2');
-    console.log(projectContainer);
-  })
-  containerProject[1].addEventListener('mouseleave', () => {
-    projectContainer.classList.remove('project2');
+    projectPreview1.classList.remove('show');
+    projectPreview2.classList.add('show');
   })
 
 
